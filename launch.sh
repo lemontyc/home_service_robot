ed898b8971f9a25e5b18a53dc708930ef8fa8678
Luis Montoya <a01208334@itesm.mx>
2020-09-15 01:27:14 +0000

    [FEAT] Added world and test_slam script

--- launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-xterm  -e  " gazebo " &
-sleep 5
-#xterm  -e  " source /opt/ros/kinetic/setup.bash; roscore" & 
-xterm  -e  " source /opt/ros/noetic/setup.bash; roscore" & 
-sleep 5
-xterm  -e  " rosrun rviz rviz" 
